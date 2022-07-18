425ed202a2a2af418dd770920b188ea528326e67
Hiago <hiagosmd@hotmail.com>
2022-07-17 21:47:23 -0300

    Improving table view

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -15,12 +15,13 @@ class ChecklistViewController: UITableViewController {
     }
 
     
-    
+    // MARK: - Table View Data Source
     override func tableView(
       _ tableView: UITableView,
       numberOfRowsInSection section: Int
     ) -> Int {
-    return 1 }
+    return 5 }
+    
     override func tableView(
       _ tableView: UITableView,
       cellForRowAt indexPath: IndexPath
@@ -28,7 +29,37 @@ class ChecklistViewController: UITableViewController {
       let cell = tableView.dequeueReusableCell(
         withIdentifier: "Checklistitem",
         for: indexPath)
-      return cell
+      // Add the following code
+      let label = cell.viewWithTag(1000) as! UILabel
+        if indexPath.row % 5 == 0 {
+          label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+          label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+          label.text = "Learn iOS development"
+        } else if indexPath.row % 5 == 3 {
+          label.text = "Soccer practice"
+        } else if indexPath.row % 5 == 4 {
+          label.text = "Eat ice cream"
+        }
+      // End of new code block
+    return cell }
+    
+    
+    
+    // MARK: - Table View Delegate
+    override func tableView(
+      _ tableView: UITableView,
+      didSelectRowAt indexPath: IndexPath
+    ){
+    if let cell = tableView.cellForRow(at: indexPath) {
+        if cell.accessoryType == .none {
+          cell.accessoryType = .checkmark
+    } else {
+          cell.accessoryType = .none
+        }
+    }
+      tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
